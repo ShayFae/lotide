7a1221d79c1a1e3557579f37453bb1c09010a615
ShayFae <ifrahnoor108@gmail.com>
2022-01-10 21:38:53 +0000

    Template literal

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍 Assertion Passed: " + actual + " === " + expected);
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🚨 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
