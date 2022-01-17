910c6f90718b3354492cf47b5386bd4b397edea7
ShayFae <ifrahnoor108@gmail.com>
2022-01-17 13:58:06 +0000

    Test added

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
     return `👍 Assertion Passed: ${actual} === ${expected}`;
-
   } else {
-    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
     return `🚨 Assertion Failed: ${actual} !== ${expected}`;
-
   }
 };
 
