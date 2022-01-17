428f57916d24e13ebb6b7cc1ee134e290ed96d6d
ShayFae <ifrahnoor108@gmail.com>
2022-01-17 04:47:52 +0000

    Test added

--- tail.js
+++ tail.js
@@ -1,17 +1,5 @@
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(assertEqual) {
-  console.log(assertEqual.slice(1));
+  return assertEqual.slice(1);
 };
 
-assertEqual(tail([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail([6]), 5);
-assertEqual(tail([]), "Hello");
+module.exports = tail;
