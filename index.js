22b0302146afac04397afb97a7cd0532e9a78c4b
ShayFae <ifrahnoor108@gmail.com>
2022-01-17 19:23:26 +0000

    Export all functions

---
+++ index.js
@@ -0,0 +1,35 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const letterPositions = require('./letterPositions');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const map = require('./map');
+const takeUntill = require('./takeUntill');
+const without = require('./without');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  map: map,
+  without: without,
+  takeUntill: takeUntill,
+  countOnly: countOnly,
+  countLetters: countLetters,
+  letterPositions: letterPositions,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+};
